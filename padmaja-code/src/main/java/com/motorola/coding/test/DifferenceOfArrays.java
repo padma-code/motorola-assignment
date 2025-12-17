bf0a0e921ef515542f3e842de31cd2938f554ab6
padma <Dhriya Mallela@DESKTOP-OF7R9FM>
2025-12-17 12:45:55 +0530

    i gave Space  i added some Alphabets and

--- padmaja-code/src/main/java/com/motorola/coding/test/DifferenceOfArrays.java
+++ padmaja-code/src/main/java/com/motorola/coding/test/DifferenceOfArrays.java
@@ -9,7 +9,7 @@ public class DifferenceOfArrays {
 		int[] array1= {1,3,15,11,2};
 		int[] array2= {23,127,235,19,8};
 		smallestDifference(array1,array2,array1.length,array2.length);
-		
+//jgkjbfh		
 	}
 	
     public  static  void smallestDifference(int A[], int B[], 
